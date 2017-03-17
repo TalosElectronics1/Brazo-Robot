9c61fe0d908b52014d5b252762b2e68a01112eec
Talos Electronics <desarrollo@taloselectronics.com>
2017-03-17 12:36:11 -0600

    V1.1
    Se agregaron funciones para mover Cada articulacion

--- Brazo_robot/Brazo_robot.ino
+++ Brazo_robot/Brazo_robot.ino
@@ -1,7 +1,12 @@
 #include<SoftwareSerial.h>
 #include <Servo.h>
+SoftwareSerial BT(2, 3); // TX, RX
 
-SoftwareSerial BT(2, 3); // RX, TX
+void Enviar_status_servos();
+int bandera=0;
+void Home();
+String Datos;
+
 Servo Servo_1;
 Servo Servo_2;
 Servo Servo_3;
@@ -10,6 +15,7 @@ Servo Servo_5;
 Servo Servo_6;
 Servo Servo_7;
 
+int Angulo=0;
 int Angulo_1 = 90;
 int Angulo_2_a = 90;
 int Angulo_2_b = 90;
@@ -19,6 +25,9 @@ int Angulo_5 = 90;
 int Angulo_6 = 90;
 char Estado;
 int Tiempo = 10;
+
+void Mover_Servo(Servo Servo_1, int Angulo_Actual, int Angulo,int Velocidad);
+void Mover_2_servos(Servo Servo__1,Servo Servo__2, int Angulo_a, int Angulo_b, int Angulo,int Velocidad);
 
 void setup()
 {
@@ -43,22 +52,24 @@ void loop()
   switch (Estado)
   {
     case 'e':
-      if (Angulo_1 < 180)
+      if (Angulo_1 > 0)
       {
-        Angulo_1++;
+        Angulo_1--;
         Servo_1.write(Angulo_1);
       }
+    
       break;
       
     case 'd':
-    if (Angulo_1 > 0)
+    if (Angulo_1 < 180)
     {
-      Angulo_1--;
+      Angulo_1++;
       Servo_1.write(Angulo_1);
     }
+    
     break;
 
-    case 'g':
+    case 'f':
     if (Angulo_2_a < 180)
     {
       Angulo_2_a++;
@@ -66,9 +77,10 @@ void loop()
       Servo_2.write(Angulo_2_a);
       Servo_3.write(Angulo_2_b);
     }
+ 
     break;
 
-    case 'f':
+    case 'g':
     if (Angulo_2_a > 0)
     {
       Angulo_2_a--;
@@ -76,6 +88,7 @@ void loop()
       Servo_2.write(Angulo_2_a);
       Servo_3.write(Angulo_2_b);
     }
+  
     break;
 
     case 'h':
@@ -84,6 +97,7 @@ void loop()
       Angulo_3++;
       Servo_4.write(Angulo_3);
     }
+ 
     break;
 
     case 'i':
@@ -100,6 +114,7 @@ void loop()
       Angulo_4++;
       Servo_5.write(Angulo_4);
     }
+ 
     break;
 
     case 'k':
@@ -108,6 +123,7 @@ void loop()
       Angulo_4--;
       Servo_5.write(Angulo_4);
     }
+
     break;
 
      case 'l':
@@ -116,6 +132,7 @@ void loop()
       Angulo_5++;
       Servo_6.write(Angulo_5);
     }
+  
     break;
 
      case 'm':
@@ -124,6 +141,7 @@ void loop()
       Angulo_5--;
       Servo_6.write(Angulo_5);
     }
+
     break;
 
      case 'n':
@@ -132,6 +150,7 @@ void loop()
       Angulo_6++;
       Servo_7.write(Angulo_6);
     }
+  
     break;
 
      case 'o':
@@ -140,16 +159,19 @@ void loop()
       Angulo_6--;
       Servo_7.write(Angulo_6);
     }
+   
     break;
 
     case 'p':
     //Funcion 1
-    Serial.println("Funcion 1");
+    Home();
+ 
     break;
 
     case 'q':
     //Funcion 2
     Serial.println("Funcion 2");
+    Home();
     break;
 
     case 'r':
@@ -157,8 +179,73 @@ void loop()
     Serial.println("Funcion 3");
     break;
     
-    
   }
   delay(Tiempo);
-
+ 
+ 
+  
 }
+
+void Mover_Servo(Servo Servo_1, int Angulo_Actual, int Angulo,int Velocidad)
+{
+  while(Angulo_Actual!=Angulo)
+  {
+    if(Angulo_Actual<Angulo)
+    {
+      Angulo_Actual++;
+    }
+    if(Angulo_Actual>Angulo)
+    {
+      Angulo_Actual--;
+    }
+      Servo_1.write(Angulo_Actual);
+  }
+     delay(Velocidad);
+}
+void Mover_2_servos(Servo Servo__1,Servo Servo__2, int Angulo_a, int Angulo_b, int Angulo,int Velocidad)
+{
+  while(Angulo_a!=Angulo)
+  {
+    if (Angulo_a < Angulo)
+      {
+        Angulo_a++;
+        Angulo_b--;
+      }
+      if (Angulo_a > Angulo)
+      {
+        Angulo_a--;
+        Angulo_b++;
+      }
+      Servo__1.write(Angulo_a);
+      Servo__2.write(Angulo_b);
+  }
+   delay(Velocidad);
+}
+  
+
+void Home()
+{
+  //Restablecer los angulos a 90
+  
+  Mover_Servo(Servo_1,Angulo_1,90,10);
+  Mover_2_servos(Servo_2,Servo_3,Angulo_2_a, Angulo_2_b, 90,10);
+  Mover_Servo(Servo_4,Angulo_3,90,10);
+  Mover_Servo(Servo_5,Angulo_4,90,10);
+  Mover_Servo(Servo_6,Angulo_5,90,10);
+  Mover_Servo(Servo_7,Angulo_6,90,10);
+
+  Angulo_1 = 90;
+  Angulo_2_a = 90;
+  Angulo_2_b = 90;
+  Angulo_3 = 90;
+  Angulo_4 = 90;
+  Angulo_5 = 90;
+  Angulo_6 = 90;
+  
+}
+void Enviar_status_servos()
+{
+   
+}
+
+
